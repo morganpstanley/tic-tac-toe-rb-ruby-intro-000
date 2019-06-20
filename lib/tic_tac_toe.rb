dd69b5011c823ae2992bc76d837d6e00204bcd4c
Morgan Stanley <morganpstanley@gmail.com>
2019-06-20 03:29:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -129,9 +129,10 @@ end
 #-------------------------------------------------------------------
 
 def play(board)
-  until over?(board) == true
+  loop do
     puts "Please enter 1-9:"
     turn(board)
+  while over?(board) = false
   end
   if won?(board) == true
     puts "Congratulations #{winner(board)}!"
