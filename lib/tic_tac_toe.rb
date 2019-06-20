ed32148cb6c485e50a32f70ac9657b7717621f76
Morgan Stanley <morganpstanley@gmail.com>
2019-06-20 03:02:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,8 +29,9 @@ end
 
 #-------------------------------------------------------------------
 
-def move(board, index)
-  board[index] = current_player(board)
+def move(board, index, token)
+  token = current_player(board)
+  board[index] = token
   return board
 end
 
@@ -61,7 +62,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, token)
     display_board(board)
   else
     turn(board)
